b57aa5f5358ae8ef5a6a24fee83864f92eaf373c
Philip Chang <sgnoohc@gmail.com>
2020-04-11 10:59:01 -0700

    update

--- src/Process_TTHH.cc
+++ src/Process_TTHH.cc
@@ -11,8 +11,7 @@ void Process_TTHH()
     // Set variables used in this category.
     // If histograms are booked with these variables the histograms will be filled automatically.
     // Please follow the convention of <category>_<varname> structure.
-    // N.B. PLEASE USE float TYPE FOR EVERYTHING, UNLESS IT IS LORENTZVECTOR (which is also done in float).
-    ana.tx.setBranch<float>("TTHH_intVar1", -999);
+    ana.tx.setBranch<int>("TTHH_intVar1", -999);
     ana.tx.setBranch<float>("TTHH_floatVar1", -999);
 
     // Example of reading from Nano
